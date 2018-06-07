6154c07f5194f3408d6b557b2d57b262911ae0e3
johnniang <1340692778@qq.com>
2018-06-07 14:48:52 +0800

    fix bug: loop login method

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -58,6 +58,7 @@ const actions = {
           type.ERROR_OCCURRED,
           'Login failed, username or password was not available'
         )
+        commit(type.CLEAR_TOKEN)
         console.error(`login error, response: ${response}`)
       }
     })
